bfd7b25f9691e33b60b891d2b2ce990e40f7d3b5
PunyaYayan <zayyancahyar@gmail.com>
2020-09-19 13:26:44 +0700

    Save Realm Half

--- app/src/main/java/com/example/myapplication/ListData.java
+++ app/src/main/java/com/example/myapplication/ListData.java
@@ -1,12 +1,14 @@
 package com.example.myapplication;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.Priority;
@@ -34,14 +36,13 @@ public class ListData extends AppCompatActivity {
         //addData();
         addDataOnline();
     }
-
     void addData() {
         //offline, isi data offline dulu
         DataArrayList = new ArrayList<>();
         Model data1 = new Model();
         data1.setOriginal_title("Judul Film");
         data1.setPoster_path("https://image.tmdb.org/t/p/w500/k68nPLbIST6NP96JmTxmZijEvCA.jpg");
-                data1.setAdult(false);
+        data1.setAdult(false);
         data1.setOverview("Deskripsi Film disini");
         data1.setVote_count(100);
         data1.setRelease_date("01-01-2020");
@@ -68,10 +69,11 @@ public class ListData extends AppCompatActivity {
 
     }
 
-    void addDataOnline (){
+    void addDataOnline(){
         AndroidNetworking.get("https://api.themoviedb.org/3/movie/now_playing?api_key=6ac7a042ac3b7599a689eb943fa0b6d0&language=en-US")
-        .setPriority(Priority.LOW)
-        .build()
+                .setTag("test")
+                .setPriority(Priority.LOW)
+                .build()
                 .getAsJSONObject(new JSONObjectRequestListener() {
                     @Override
                     public void onResponse(JSONObject response) {
@@ -85,22 +87,30 @@ public class ListData extends AppCompatActivity {
                             JSONArray jsonArray = response.getJSONArray("results");
                             Log.d("hasiljson2", "onResponse: " + jsonArray.toString());
                             for (int i = 0; i < jsonArray.length(); i++) {
-
                                 modelku = new Model();
                                 JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                modelku.setId(jsonObject.getInt("id"));
                                 modelku.setOriginal_title(jsonObject.getString("original_title"));
                                 modelku.setOverview(jsonObject.getString("overview"));
                                 modelku.setRelease_date(jsonObject.getString("release_date"));
-                                modelku.setPoster_path("https://image.tmdb.org/t/p/w500/" + jsonObject.getString("poster_path"));
+                                modelku.setPoster_path("https://image.tmdb.org/t/p/w500"+jsonObject.getString("poster_path"));
                                 modelku.setAdult(jsonObject.getBoolean("adult"));
                                 modelku.setVote_count(jsonObject.getInt("vote_count"));
                                 DataArrayList.add(modelku);
                             }
-                            // Untuk Handle click
+                            //untuk handle click
                             adapter = new DataAdapter(DataArrayList, new DataAdapter.Callback() {
                                 @Override
                                 public void onClick(int position) {
-
+                                    Model movie = DataArrayList.get(position);
+                                    Intent intent = new Intent(getApplicationContext(), DetailMovie.class);
+                                    intent.putExtra("id",movie.id);
+                                    intent.putExtra("judul",movie.original_title);
+                                    intent.putExtra("date",movie.release_date);
+                                    intent.putExtra("deskripsi",movie.overview);
+                                    intent.putExtra("path",movie.poster_path);
+                                    startActivity(intent);
+                                    Toast.makeText(ListData.this, ""+position, Toast.LENGTH_SHORT).show();
                                 }
 
                                 @Override
@@ -114,15 +124,17 @@ public class ListData extends AppCompatActivity {
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
+
                     }
 
-                        @Override
-                        public void onError(ANError error) {
-                            // handle error
-                            Log.d("errorku", "onError errorCode : " + error.getErrorCode());
-                            Log.d("errorku", "onError errorBody : " + error.getErrorBody());
-                            Log.d("errorku", "onError errorDetail : " + error.getErrorDetail());
-                        }
-                    });
-                }
+                    @Override
+                    public void onError(ANError error) {
+                        // handle error
+                        Log.d("errorku", "onError errorCode : " + error.getErrorCode());
+                        Log.d("errorku", "onError errorBody : " + error.getErrorBody());
+                        Log.d("errorku", "onError errorDetail : " + error.getErrorDetail());
+                    }
+                });
     }
+
+}
